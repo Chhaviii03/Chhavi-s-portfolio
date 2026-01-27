7ad574107ab47d5e16912a19cf3648596cdc4f5d
Chhavi Bhatt <chhavipradeepbhatt@gmail.com>
2026-01-28 02:47:09 +0530

    port changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import express from 'express'
 import contactRoutes from './routes/contact.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
 // Middleware
 app.use(cors())
